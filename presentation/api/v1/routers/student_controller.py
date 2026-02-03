e51ab612ff4f50d4b0534c7b9d1c86892faea183
GuillermoSM33 <guillermo.jesus.garcia.canul@gmail.com>
2026-02-02 22:35:19 -0500

    Fix(Student_Controller) Fix for submit excelL

--- presentation/api/v1/routers/student_controller.py
+++ presentation/api/v1/routers/student_controller.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
+
+from infrastructure.persistence.repositories.db import get_db
 from application.dtos.students.get_student_dto import GetStudentDetailDTO
 from application.dtos.students.update_student_dto import UpdateStudentDTO
-from infrastructure.persistence.repositories.db import get_db
-from application.services.student_service import import_students_from_xls, get_all_students_service, import_students_from_xls, update_student_service
-from typing import List, Any
+from application.services.student_service import (
+    get_all_students_service,
+    update_student_service,  
+    import_students_from_excel,   
+)
 
 router = APIRouter()
 
@@ -21,9 +26,9 @@ def update_student(user_id: int, student_data: UpdateStudentDTO, db: Session = D
 @router.post("/students/import_xls")
 async def import_students_xls(file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
-        file.file.seek(0)
-        content = file.file.read()
-        results = import_students_from_xls(db, content, create_auth=False)
+        content = await file.read()
+        results = import_students_from_excel(db, content, create_auth=False)
         return results
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error importing XLS: {e}")
+        raise HTTPException(status_code=400, detail=f"Error importing XLSX: {e}")
+
